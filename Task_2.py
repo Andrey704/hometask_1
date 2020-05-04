8985c59293c3dd81b64a7fca47b988d449534889
Andrey Mochalov <mochalovandrew39@gmail.com>
2020-05-05 02:12:31 +0300

    Another task

---
+++ Task_2.py
@@ -0,0 +1,8 @@
+print ("Please enter the salary of three employees in figures ")
+salary1 = input ("Enter first salary: ")
+salary2 = input ("Enter second salary: ")
+salary3 = input ("Enter third salary: ")
+
+avarage = (int(salary1) + int(salary2) + int(salary3))/3
+
+print("Avarage salary is " + str(avarage))
