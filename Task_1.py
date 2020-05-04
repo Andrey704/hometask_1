8cdf19f50a9c465add5dad01062d1b11ccad5467
Andrey Mochalov <mochalovandrew39@gmail.com>
2020-05-05 01:41:07 +0300

    First version

---
+++ Task_1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+a = input ("How old are you? ")
+b = input ("Please, enter the year: ")
+
+c = int(b) - (2020 - int(a))
+
+print (name.title()+ " your age in " + str(b) + " will be " + str(c))
